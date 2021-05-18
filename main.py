ab23dd81ac02b31167970261e0f9403baca3b7cb
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2021-05-18 11:25:49 -0500

    Search Feature Implemented
    Now allows users to search for their previous login information by typing in website and clicking search.

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
-# generating random password
+
+# GENERATING RANDOM PASSWORD
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+               'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+               'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -13,7 +18,7 @@ def generate_password():
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
 
-                     # [new_item for item in list]
+    # [new_item for item in list]
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
     password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
     password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
@@ -30,27 +35,62 @@ def generate_password():
     pyperclip.copy(password)
 
 
+# FIND PASSWORD
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("login_info.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"There are no details for {website}.")
+
+
+# SAVING PASSWORD
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
     # checking if the user left any fields blank
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="ERROR", message="Please make sure there are not any empty fields.")
     else:
-        # message box that makes user check their info
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                              f"\nEmail/Username: {email}\nPassword: {password}"
-                                                              f"\nIs it okay to save?")
-        if is_ok:
-            with open("login_info.txt", "a") as login_data:
-                login_data.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                email_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("login_info.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("login_info.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+
+            with open("login_info.json", "w") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
+# USER INTERFACE
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -69,8 +109,8 @@ password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0)
 
 # entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -79,6 +119,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
